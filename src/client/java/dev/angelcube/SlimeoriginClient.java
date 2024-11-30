81919be13695bb411a18c8840a111f58699f7597
Angel <144711471+weightedangelcube@users.noreply.github.com>
2024-11-30 08:28:30 -0700

    quick reset
    so we can also try working from a blanker slate

---
+++ src/client/java/dev/angelcube/SlimeoriginClient.java
@@ -0,0 +1,10 @@
+package dev.angelcube;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SlimeoriginClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
