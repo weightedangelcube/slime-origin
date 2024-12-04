993e874c9f5776e4c09df20a6a9f40a673e74a4d
weightedangelcube <angel@angelcube.dev>
2024-12-04 02:41:32 +0000

    fix deps

--- src/main/java/dev/angelcube/Slimeorigin.java
+++ src/main/java/dev/angelcube/Slimeorigin.java
@@ -5,13 +5,35 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Slimeorigin implements ModInitializer {
-	public static final String MOD_ID = "slimeorigin";
+import io.github.apace100.apoli.power.Power;
+import io.github.apace100.apoli.power.type.PowerType;
+import io.github.apace100.apoli.power.type.PowerReference;
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.ClampedEntityAttribute;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class Slimeorigin implements ModInitializer {
+    public static final String MOD_ID = "slimeorigin";
+
+    public static final PowerType<Power> BOUNCY = new PowerReference<Power>(new Identifier(MOD_ID, "bouncy"));
+    public static final PowerType<Power> FRAGMENTATION = new PowerReference<Power>(new Identifier(MOD_ID, "fragmentation"));
+
+    public static final EntityAttribute SLIME_SIZE = make("slime_size", 0, 0 ,3);
+
+    public static double getSlimeSize(final LivingEntity entity){
+        final EntityAttributeInstance size = entity.getAttributeInstance(SLIME_SIZE);
+        return size.getValue();
+    }
+
+
+    private static EntityAttribute make(final String name, final double base, final double min, final double max) {
+        return new ClampedEntityAttribute("attribute.name.generic." + MOD_ID + '.'  + name, base, min, max).setTracked(true);
+    }
+
 
 	@Override
 	public void onInitialize() {
@@ -19,6 +41,5 @@ public class Slimeorigin implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
