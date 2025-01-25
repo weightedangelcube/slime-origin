12adcf3236b3a46fa7c1b01dcd9a36b8dfe490c6
Angel <144711471+weightedangelcube@users.noreply.github.com>
2025-01-25 12:20:10 -0700

    chore: bump Origins and Fabric API versions
    Refactored PowerConfiguration registers to remove deprecated methods

--- src/main/java/dev/angelcube/slimeorigin/power/type/SlimePowerTypes.java
+++ src/main/java/dev/angelcube/slimeorigin/power/type/SlimePowerTypes.java
@@ -4,6 +4,7 @@ import dev.angelcube.slimeorigin.Slimeorigin;
 
 import io.github.apace100.apoli.power.PowerConfiguration;
 import io.github.apace100.apoli.power.type.PowerType;
+import io.github.apace100.apoli.power.type.PowerTypes;
 import io.github.apace100.apoli.registry.ApoliRegistries;
 
 import net.minecraft.registry.Registry;
@@ -16,25 +17,13 @@ public class SlimePowerTypes {
      * Creates new PowerConfigurations BOUNCINESS and FRAGMENTATION, and registers them to Apoli's
      * registry.
      */
-    public static final PowerConfiguration<BouncinessPowerType> BOUNCINESS = register(
-        PowerConfiguration.dataFactory(Slimeorigin.identifier("bounciness"), BouncinessPowerType.DATA_FACTORY)
+    public static final PowerConfiguration<BouncinessPowerType> BOUNCINESS = PowerTypes.register(
+        PowerConfiguration.of(Slimeorigin.identifier("bounciness"), BouncinessPowerType.DATA_FACTORY)
     );
-    public static final PowerConfiguration<FragmentationPowerType> FRAGMENTATION = register(
+    public static final PowerConfiguration<FragmentationPowerType> FRAGMENTATION = PowerTypes.register(
         PowerConfiguration.simple(Slimeorigin.identifier("fragmentation"), FragmentationPowerType::new)
     );
 
-    /**
-     * Registers a provided PowerConfiguration to Apoli's registry.
-     *
-     * @param configuration The configuration to register
-     * @return The registered configuration
-     */
-    @SuppressWarnings("unchecked")
-    public static <T extends PowerType> PowerConfiguration<T> register(PowerConfiguration<T> configuration) {
-        PowerConfiguration<PowerType> casted = (PowerConfiguration<PowerType>) configuration;
-        Registry.register(ApoliRegistries.POWER_TYPE, casted.id(), casted);
-        return configuration;
-    }
 
     /**
      * Registers all the PowerTypes defined in this class. Apoli does this for us, so we can just
